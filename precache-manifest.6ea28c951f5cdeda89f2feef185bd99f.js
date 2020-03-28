cac2156ddb937ce042b6ae9e9d1b2af9c231dc22
Lisbaldy <lisba7474@gmail.com>
2020-03-28 17:17:57 -0300

    Updates

---
+++ precache-manifest.6ea28c951f5cdeda89f2feef185bd99f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "255b3c4f35acacac224fbb66e0465b2f",
+    "url": "/test-with-react/index.html"
+  },
+  {
+    "revision": "eaa32a57c8c17e89df80",
+    "url": "/test-with-react/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "10bf244ca692441702db",
+    "url": "/test-with-react/static/js/2.1b3a0643.chunk.js"
+  },
+  {
+    "revision": "eaa32a57c8c17e89df80",
+    "url": "/test-with-react/static/js/main.1fe9e88e.chunk.js"
+  },
+  {
+    "revision": "084cb247f94c3b3fd88d",
+    "url": "/test-with-react/static/js/runtime~main.c521e0bf.js"
+  }
+]);
